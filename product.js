7550f29af64d7d3610078f2dfb749a3c8ae960e0
OUKHATOU HAMZA <hamzaoukhatou@gmail.com>
2024-12-17 14:38:10 +0100

    ajouter le fichier de classe de produit

---
+++ product.js
@@ -0,0 +1,10 @@
+// Définir la classe Product
+class Product {
+    constructor(name, quantity, description, prix) {
+        this.name = name;
+        this.quantity = quantity;
+        this.description = description;
+        this.prix = prix;
+    }
+   
+}
