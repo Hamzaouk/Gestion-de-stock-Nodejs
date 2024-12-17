efe405d757bfef8eabc566dca2ae2d7a377dcee7
OUKHATOU HAMZA <hamzaoukhatou@gmail.com>
2024-12-17 12:23:09 +0100

    first commit

---
+++ inventory.js
@@ -0,0 +1,8 @@
+// Importer readline module
+const readline = require("readline");
+
+// Configurer les entrées/sorties
+const read = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
