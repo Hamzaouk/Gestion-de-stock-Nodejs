8396276e68a1b1205ece5c6c5981b37d4fcc6699
OUKHATOU HAMZA <hamzaoukhatou@gmail.com>
2024-12-20 12:58:18 +0100

    le menu d'affichage les options

--- inventory.js
+++ inventory.js
@@ -1,8 +1,24 @@
 // Importer readline module
 const readline = require("readline");
+const Product = require("./product"); // Import the Product class
 
 // Configurer les entrées/sorties
 const read = readline.createInterface({
     input: process.stdin,
     output: process.stdout,
 });
+
+class Inventory {
+    
+    menu() {
+        console.log("\n--- Menu ---\n");
+        console.log("1. Ajouter un produit au stock");
+        console.log("2. Afficher tous les produits en stock");
+        console.log("3. Mettre à jour un produit");
+        console.log("4. Supprimer un produit du stock");
+        console.log("5. Quitter");
+        read.question("Choisissez une option: ", (choix) => this.gererChoix(choix));
+    }
+
+
+}
--- product.js
+++ product.js
@@ -1,10 +1,14 @@
 // Définir la classe Product
 class Product {
+    static nextId = 1; 
+
     constructor(name, quantity, description, prix) {
+        this.id = Product.nextId++; // next id + 1 pour next id 
         this.name = name;
         this.quantity = quantity;
         this.description = description;
         this.prix = prix;
     }
-   
 }
+
+module.exports = Product;
